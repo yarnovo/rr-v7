8195b2d7b799e3822f07dcf3ab466ac58032cc8c
Terry Sutton <saltcod@gmail.com>
2025-03-02 08:56:34 -0330

    Cleanup

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import type { Route } from './+types/root'
 import './app.css'
 import { createClient } from './lib/supabase.server'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -70,7 +71,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />
+  return (
+    <>
+      <Outlet />
+      <Footer />
+    </>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
